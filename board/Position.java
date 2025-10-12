25b8fa1a7850e1cd636a1fc7b34d6557ce42a555
cfi19 <cfi19@txstate.edu>
2025-10-12 17:43:09 -0500

    Made modifications to Board/Game && Position.java, players/Player.java, utils/Notation.java, and App.java

--- board/Position.java
+++ board/Position.java
@@ -1,5 +1,30 @@
 package Board;
 
+/*
+ * LEGEND:
+ * row 0 = top of board (rank 8)
+ * row 7 = bottom of board (rank 1)
+ * 
+ * col 0 = file 'A'
+ * col 7 = file 'H'
+ */
 public class Position {
+    private int row;
+    private int col;
+
+    //CONSTRUCTOR
+    public Position(int row, int col){
+        this.row = row;
+        this.col = col; 
+    }//end of CONSTRUCTOR
+
+    //GETTERS
+    public int getRow(){
+        return row;
+    }//end of int getRow
+
+    public int getCol(){
+        return col;
+    }//end of int getCol
     
 }
